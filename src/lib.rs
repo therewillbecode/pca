820026bc801f86ed73bafcf859b9c5e749aa81a4
therewillbecode <tomw08@gmail.com>
2023-09-23 08:08:57 +0100

    Alias type

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,14 @@
-use nalgebra::{vector, DMatrix, DVector, Dyn, Dynamic, OMatrix, Vector3};
+use nalgebra::{vector, DMatrix, DVector, Dynamic, OMatrix, Vector3};
 
 // Alias for Dynamically sized and dynamically allocated matrix.
-type MatrixF64 = OMatrix<f64, Dyn, Dyn>;
+type MatrixF64 = DMatrix<f64>;// OMatrix<f64, Dyn, Dyn>;
 
 /// Rows are observations, columns are variables/features.
 pub fn rand_matrix() -> MatrixF64 {
     DMatrix::new_random(3, 3)
 }
 
-pub fn normalise(mut m: OMatrix<f64, Dyn, Dyn>) -> MatrixF64 {
+pub fn normalise(mut m:MatrixF64) -> MatrixF64 {
     for mut col in m.column_iter_mut() {
         let normalized = col.normalize(); // Help WHat does this actually do?
                                           // What does it mean to normalise data?
@@ -21,7 +21,7 @@ pub fn normalise(mut m: OMatrix<f64, Dyn, Dyn>) -> MatrixF64 {
 // AKA column centering (columns are variables).
 // We just move the origin of the coordinate system to coincide
 // with the average value for each dimension.
-pub fn center_at_mean(m: OMatrix<f64, Dyn, Dyn>) -> MatrixF64 {
+pub fn center_at_mean(m: MatrixF64) -> MatrixF64 {
     // Get columns
     //  let rows = m.columns_generic_mut(0, m.ncols());
 
@@ -39,8 +39,8 @@ pub fn center_at_mean(m: OMatrix<f64, Dyn, Dyn>) -> MatrixF64 {
 
     let means_row: DVector<f64> = DVector::from_vec(col_means.clone());
 
-    let centering_matrix: OMatrix<f64, Dyn, Dyn> =
-        OMatrix::<f64, Dyn, Dyn>::from_fn(num_rows, m.ncols(), |_r, c| col_means[c]);
+    let centering_matrix: MatrixF64 =
+      MatrixF64::from_fn(num_rows, m.ncols(), |_r, c| col_means[c]);
     println!("cen {}", centering_matrix.clone());
 
     let mean_centered = m.clone() - centering_matrix;
@@ -74,8 +74,8 @@ pub fn center_at_mean(m: OMatrix<f64, Dyn, Dyn>) -> MatrixF64 {
 /// Cov can be anything from -inf to +inf
 /// Also remember covariance is sensitive to scaling unlike
 /// the correlation coefficient.
-fn get_covariance_matrix(m: OMatrix<f64, Dyn, Dyn>) {
-    todo!()
+fn get_covariance_matrix(m: MatrixF64) {
+   //m.cov()
 }
 
 // V_1^T * R^T * R * v1
